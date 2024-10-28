c326699b4ff2b0a9dc471d432c2a735178e24189
eweggel74 <erw74@drexel.edu>
2024-10-28 13:22:19 -0400

    Initial main commit to make basic express/axios project. Added node modules, gitignore and psql startup with schema.

---
+++ sql/setup.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE datcord;
+
+\c datcord
+
+CREATE TABLE IF NOT EXISTS Users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(16),
+    hashedPassword VARCHAR(256),
+    bio VARCHAR(190),
+    status VARCHAR(16),
+    birthday DATE
+);
