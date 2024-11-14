86596d50003303bf53d032f5ecc7e889b896d79b
Zachary <zacharyhopp01@gmail.com>
2024-11-14 12:15:17 -0500

    Fixed json promise error in the client by adding .json({}) to login handler response

--- app/apiServer.js
+++ app/apiServer.js
@@ -424,7 +424,7 @@ app.post("/login", async (req, res) => {
         let token = makeToken();
         let hashedToken = await hashItem(token);
         saveToken(username, hashedToken);
-        return res.status(200).cookie("token", token, cookieOptions).send();
+        return res.status(200).cookie("token", token, cookieOptions).json({}).send();
     } else {
         return res.json({"error": "Missing login properties"});
     }
